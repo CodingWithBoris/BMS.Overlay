a3fda49cb5dc131846c70cb5eb988a19f06e397b
CodingWithBoris <boris60601@gmail.com>
2026-02-25 20:38:11 +0100

    Quintuple Boom

--- obj/Release/net8.0-windows/win-x64/BMS.Overlay.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/BMS.Overlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BMS.Overlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+194e0b644d25e0f946a932e57e8dd3ce81134a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642c22a421c56169ac8b099de6f4963e7f573d74")]
 [assembly: System.Reflection.AssemblyProductAttribute("BMS.Overlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BMS.Overlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
